d058fc3cffcc564e63e635d380deb18f0ad0e7b2
Syifa <syifaingez200705@gmail.com>
2022-10-27 11:43:06 +0700

    remed ujian lisan

---
+++ Remedial_Syifa.js
@@ -0,0 +1,39 @@
+//for
+for(x=1; x<=12; x++)
+{
+	console.log("FOR");
+}
+
+//while
+var x = 0;
+	while(x < 5)
+	{
+		console.log("While");
+		x++;
+	}
+
+//do while
+var x = 0;
+    do {
+        console.log("Do While");
+        x++;
+    }
+    while (x < 7)
+
+//assignment
+var x = 8;
+var y = 5;
+
+console.log(x);
+
+console.log(x = y + 1);
+
+console.log(x = x * y); 
+
+//comparison
+var s = 8;
+var r = '7';
+  
+    console.log(r == 5);
+    console.log(s == 5);        
+    console.log(r == r);
